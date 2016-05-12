e64bda8d404ab2da049e3fd9fc6830af5fdc4d65
unknown <tyam@N34125.office.amsiag.com>
2016-05-12 17:57:55 +0900

    Updated.

--- cordic_sqrt_seq/scr/truncation_error.py
+++ cordic_sqrt_seq/scr/truncation_error.py
@@ -1,11 +1,17 @@
 import math
 
 arr = []
-arr.append([math.atanh(2**i) for i in range(-1,  -5, -1)])
-arr.append([math.atanh(2**i) for i in range(-4,  -9, -1)])
+arr.append([math.atanh(2**i) for i in range(-1, -5, -1)])
+arr.append([math.atanh(2**i) for i in range(-4, -9, -1)])
 
 print(2.0**-15)
 print(math.atanh(2**-8))
 print(math.cosh(math.atanh(2**-8))-1.0)
 for i in range(len(arr)):
   print (sum(arr[i]), arr[i][0] - sum(arr[i][1:]))
+
+coeff = \
+ reduce( lambda x, y: x*y
+       , [ 1/math.cosh(math.atanh(2**i))
+           for i in range(-1, -5, -1) + range(-4, -9, -1)] )
+print (coeff, coeff*2**19, "%X" % math.floor(coeff*2**19))
